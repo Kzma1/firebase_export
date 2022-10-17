1feb7ba26b96274d99f2805fe08bc80306c2a15b
Kzma1 <71949462+Kzma1@users.noreply.github.com>
2022-10-17 09:51:12 +0800

    refactor: :bulb: add-comment

--- export.ts
+++ export.ts
@@ -1,21 +1,18 @@
-import { applicationDefault, cert, initializeApp } from 'firebase-admin/app';
+import { applicationDefault, initializeApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore';
-import { ServiceAccount } from 'firebase-admin/app';
-import serviceAccount from './key/export-firebase-csv-firebase-adminsdk.json';
 
 import fs from 'fs';
 
 type Target = 'firestore' | 'authentication';
 initializeApp({
-  // credential: applicationDefault()
-  credential: cert(serviceAccount as ServiceAccount)
+  credential: applicationDefault()
 });
 
 let target: Target = 'firestore';
 
 const getFirestoreDocuments = async () => {
-  const collectionName = process.argv[2]; //ターミナルコマンドの2番目の引数を取ってきます
+  const collectionName = process.argv[2]; // Takes the second argument of the terminal command
   const db = getFirestore();
   const querySnapshot = await db.collection(collectionName).get();
   const queryDocumentSnapshot = querySnapshot.docs;
@@ -23,6 +20,7 @@ const getFirestoreDocuments = async () => {
   return queryDocumentSnapshot.map((document) => {
     const documentData = document.data();
 
+    // Change to match the fields in the document
     return {
       name: documentData.name
     };
@@ -45,7 +43,7 @@ const exportJson = (data: Object) => {
     if (err) {
       return console.log(err);
     }
-    console.log('JSONファイルのエクスポート成功!');
+    console.log('success!');
   });
 };
 
@@ -60,7 +58,8 @@ const main = async () => {
       break;
   }
   const keys = Object.keys(data[0]);
-  console.log('csv変換時にコマンドで使用する', keys.toString());
+  console.log('Document Key:', keys.toString()); // Used in CLI when csv conversion
+
   exportJson(data);
 };
 
